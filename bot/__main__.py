66564bbdc24214601e93836dd36b73f4e09bbf08
MR.SHAGGY <87988393+mrSHAGGYboy@users.noreply.github.com>
2021-09-05 12:01:38 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -36,9 +36,9 @@ if not os.path.isdir("./downloads"):
 
 Bot.start()
 User.start()
-print("\nVideo Player Bot Started, Join @sl_bot_zone !")
+print("\nVideo Player Bot Started, Join @SL_MEDIA_TECH_GRUOP !")
 
 idle()
 Bot.stop()
 User.stop()
-print("\nVideo Player Bot Stopped, Join @sl_bot_zone !")
+print("\nVideo Player Bot Stopped, Join @SL_MEDIA_TECH_GRUOP !")
