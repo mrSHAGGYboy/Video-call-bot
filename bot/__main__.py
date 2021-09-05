705d422f805c6fa2811a328532af24bf5d643615
MR.SHAGGY <87988393+mrSHAGGYboy@users.noreply.github.com>
2021-09-05 12:14:58 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,29 +16,21 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
-import os
-import sys
-import asyncio
 from pyrogram import Client, idle
-from config import Config
-from bot.safone.nopm import User
-from pyrogram.raw import functions, types
+from config import API_ID, API_HASH, BOT_TOKEN
+from bot.videoplayer import app
 
-Bot = Client(
+
+bot = Client(
     ":memory:",
-    Config.API_ID,
-    Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="bot.safone"),
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot"),
 )
-if not os.path.isdir("./downloads"):
-    os.makedirs("./downloads")
 
-Bot.start()
-User.start()
-print("\nVideo Player Bot Started, Join @SL_MEDIA_TECH_GRUOP !")
-
+bot.start()
+print("[INFO]: STARTING BOT CLIENT")
+app.start()
+print("[INFO]: STARTING USERBOT CLIENT")
 idle()
-Bot.stop()
-User.stop()
-print("\nVideo Player Bot Stopped, Join @SL_MEDIA_TECH_GRUOP !")
